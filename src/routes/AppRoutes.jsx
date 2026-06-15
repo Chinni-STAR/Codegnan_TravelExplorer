4d19a434143b10bd9b538f9eca08096dfd26278a
Chinni-STAR <chinnimusirana@gmail.com>
2026-06-15 23:09:44 +0530

    Travel Explorer

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import AddDestination from "../pages/AddDestination";
+import {AddDestination} from "../pages/AddDestination";
 
 import Home from "../pages/Home";
 import Destinations from "../pages/Destinations";
